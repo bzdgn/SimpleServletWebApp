7a7432bff674f27aa54df0b2e2e05a23d502e80c
Levent Divilioglu <levent.divilioglu@pia-team.com>
2014-10-31 10:06:59 +0200

    - Raw String, Xml, JSon outputs

--- src/main/java/com/levent/SimpleServlet.java
+++ src/main/java/com/levent/SimpleServlet.java
@@ -17,6 +17,21 @@ public class SimpleServlet extends HttpServlet
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws ServletException, IOException
 	{
-		resp.getWriter().write("Naber lan");
+		//simple string return
+		//resp.getWriter().write("Naber lan");
+		
+		//Sample Xml Output
+		String outputXmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+		
+		outputXmlString += "<reply><id>Naber lan</id><message>Forza FENER!</message></reply>";
+		
+		//Sample Json Output
+		//As is;
+		//String outputJsonString = "{\"id\":\"1907\",\"message\":\"En buyuk fener!\"}";
+		
+		//Pretty Print
+		//String outputJsonString = "{\n\t\"id\":\"1907\",\n\t\"message\":\"En buyuk fener!\"\n}";
+		
+		resp.getWriter().write(outputXmlString);
 	}
 }
